06ac2a157df8871ae41a541bccb58fabec654259
Iren1Paramonova <zerber17@ya.ru>
2022-12-17 22:21:01 +0300

    17/12

---
+++ src/TextContent.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const TextContent = () => {
+  return (
+    <div>
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+      <p>HELLO WORLD.</p>
+    </div>
+  );
+};
+export default TextContent;
