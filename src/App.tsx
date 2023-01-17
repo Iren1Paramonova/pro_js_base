d60729a53a8814de1627e5b0ee4f7a109cd129c4
Iren1Paramonova <zerber17@ya.ru>
2023-01-17 21:32:05 +0300

    17/01/23

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
+import { Main } from 'Containers/Main';
 import React from 'react';
 import './App.css';
-import { CounterComponent } from './CounterComponent';
-import { Input } from './Input';
 
 function App() {
   const handler = () => {
     console.log('Кнопка нажата');
   };
-  return (
-    <div className="App">
-      <CounterComponent title={'Orange'} count={9} />
-      <CounterComponent title={'Apple'} count={4} />
-      <CounterComponent title={'Lemon'} count={3} />
-      <button onClick={handler}> Push me</button>
-      <Input />
-
-      <img src={'./img.jpg'} alt="img" />
-      <div className="example">HELLO</div>
-    </div>
-  );
+  return <Main />;
 }
 
 export default App;
---
+++ src/Components/Common/Header/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import style from './Header.module.scss';
+
+export const Header = () => {
+  return <header className={style.header}>Header</header>;
+};
